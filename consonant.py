4b6c8f00b2f0f7e7cf544dc8636ce965c4ba3b6e
Farubegum <42862209+Farubegum@users.noreply.github.com>
2019-02-07 10:14:37 +0530

    Create consonant.py

---
+++ consonant.py
@@ -0,0 +1,5 @@
+sa= input("enter the character :")
+if(sa=='A' or sa=='a' or sa=='E' or sa=='e' or sa=='O' or     sa=='o' or sa=='I' or sa=='i' or sa=='U' or sa=='u'):
+  print(sa, "is a Vowel")
+else:
+  print(sa, "is a Consonant")
